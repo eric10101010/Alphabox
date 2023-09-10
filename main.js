cf82a0fc49a01ad99dd45e7aba71912cf28d2e5d
eric10101010 <erickaivalue@gmail.com>
2023-09-10 23:55:15 +0800

    update

---
+++ main.js
@@ -0,0 +1,43 @@
+import './assets/scss/all.scss';
+
+$(document).ready(function(){
+	$('.hamburger').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+const swiperIntro = new Swiper('.swiperAbout', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	autoplay: {
+	delay: 5000,
+},
+	breakpoints: {
+	768: {
+		autoplay: {
+		enabled: true,
+		}
+	}
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+var swiper = new Swiper(".swiperUse", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints:{
+		768:{
+		slidesPerView: 2,
+		},
+		992:{
+		slidesPerView: 3,
+		}
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		dragSize:554,
+	},   
+});
